55896d39466c9ae80750d76ddca7906f2cce24c2
student-pradyumna <162539396+student-pradyumna@users.noreply.github.com>
2025-09-18 19:20:41 +0530

    Update server.js

--- oneCart/backend/server.js
+++ oneCart/backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://e-commerce-onecart-myfrontend-h81m.onrender.com", "http://localhost:5174"],
     credentials: true,
   })
 );
