9447deb3302dd3aec6bfe8b8cfa7ab670dd0078c
student-pradyumna <162539396+student-pradyumna@users.noreply.github.com>
2025-09-18 13:06:04 +0530

    Update userRoutes.js

--- oneCart/backend/routes/userRoutes.js
+++ oneCart/backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import isAuth from '../middleware/isAuth.js'
 import { getCurrentUser ,getAdmin} from '../controllers/userController.js'
-import adminAuth from '../middleware/AdminAuth.js';
+import adminAuth from '../middleware/adminAuth.js';
 let userRoutes=express.Router()
 
 
